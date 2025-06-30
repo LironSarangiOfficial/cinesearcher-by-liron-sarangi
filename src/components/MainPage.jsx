3b2187623f7a35546388324d226c3974c411cd69
Liron Sarangi <sarangi.liron7@gmail.com>
2025-07-01 01:56:56 +0530

    Fixed: Mistakenly unimported MovieList from MainPage component

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 import MovieHistoryList from "./movieHistory";
+import MovieList from "./movieList";
 
 const MainPage = () => (
   <div className="flex flex-row">
-    {/* <MovieList /> */}
+    <MovieList />
     <MovieHistoryList />
   </div>
 );
