7f4299299a059581e26d24532b580c08bd1530f0
Liron Sarangi <sarangi.liron7@gmail.com>
2025-07-01 17:00:30 +0530

    Removed unnecessary console.log from movieApi.js

--- src/apis/movieApi.js
+++ src/apis/movieApi.js
@@ -2,10 +2,6 @@ import { OMDB_API_URL } from "constants";
 
 import axios from "axios";
 
-// const apiKey = process.env.REACT_APP_OMDB_API_KEY
-// const API_BASE_URL = "https://www.omdbapi.com"
-
 const fetchMovie = params => axios.get(OMDB_API_URL, { params });
-// const fetchMovie = params => axios.get("", { params });
 
 export default { fetchMovie };
